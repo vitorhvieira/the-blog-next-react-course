c42f177bae00963dc7c06212baad664b94fd8cae
unknown <vitorhugovieirabraga@gmail.com>
2025-04-26 17:18:19 -0300

    Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
+import { Container } from "@/components/Container";
 import { PostList } from "@/components/PostList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function Home() {
   return (
-    <div className="text-slate-900 bg-slate-100 min-h-screen">
+    <Container>
       <header>
-        <h1 className="font-bold text-9xl text-center">Aqui é o Header</h1>
+        <h1 className="font-bold text-6xl text-center">Aqui é o Header</h1>
       </header>
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
       <footer>
-        <h1 className="font-bold text-9xl text-center">Aqui é o Footer</h1>
+        <h1 className="font-bold text-6xl text-center">Aqui é o Footer</h1>
       </footer>
-    </div>
+    </Container>
   );
 }
